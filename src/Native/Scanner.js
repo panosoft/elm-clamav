bf16b76a9bffdeb5d37b0c490358d92595cfed8d
Don Gaudino <don@panosoft.com>
2017-07-18 14:34:03 -0700

    Added debug parameter.  Changed fail return value.

--- src/Native/Scanner.js
+++ src/Native/Scanner.js
@@ -3,24 +3,24 @@ var _panosoft$elm_clamav$Native_Scanner = function() {
     const clamav = require('clamav.js');
     const { nativeBinding, succeed, fail } = _elm_lang$core$Native_Scheduler
 
-    const scan = F3((config, name, buffer) =>
+    const scan = F4((config, name, buffer, debug) =>
         nativeBinding(callback => {
             try {
 
-                console.log({config: config, name: name, buffer_length: buffer.length,
-                    bufferAsString: buffer.toString(null, 0, (buffer.length > 100 ? 100 : buffer.length))});
+                debug ? console.log('NATIVE --', {config: config, name: name, buffer_length: buffer.length,
+                    bufferAsString: buffer.toString('hex', 0, (buffer.length > 80 ? 80 : buffer.length))}) : null;
                 const bufferStream = new stream.PassThrough();
                 bufferStream.end(buffer);
                 clamav.createScanner(config.clamavPort, config.clamavHost).scan(bufferStream, (err, object, malicious) => {
-                    console.log({name: name, err: err, malicious: malicious});
+                    debug ? console.log('NATIVE --', {name: name, err: err, malicious: malicious}) : null;
                     callback(err
-                    	? fail('Error scanning ' + name + ':' + err.message)
-                    	: (malicious ? fail('Virus found scanning ' + name + ':' + malicious) : succeed(name)))
-                });
+                        ? fail(_elm_lang$core$Native_Utils.Tuple2(name, 'Error scanning ' + name + ':' + err.message))
+                        : (malicious ? fail(_elm_lang$core$Native_Utils.Tuple2(name, 'Virus found scanning ' + name + ':' + malicious)) : succeed(name)))
+                    });
             }
             catch (error) {
-            	callback(fail('Error scanning ' + name + ':' + err.message));
-        	 }
+                callback(fail(_elm_lang$core$Native_Utils.Tuple2(name, 'Error scanning ' + name + ':' + err.message)));
+            }
         }));
         return { scan };
 }();
