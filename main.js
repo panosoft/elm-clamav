63a66c6a935e1bc3f8052ddaf190f2670795612a
Don Gaudino <don@panosoft.com>
2017-08-16 11:41:42 -0700

    Changed parameter name.  Added comments.

--- main.js
+++ main.js
@@ -3,13 +3,19 @@
 
 // load Elm module
 const elm = require('./elm.js');
+// run with:
+//  node main.js targetFilename configFilename --debug
+//
+//      targetFilename = name of file to scan.
+//      configFilename = filename for ClamAV config. "" defaults to ./sampleConfig.js.  must follow 'node require' syntax (i.e. precede with ./ if in current directory).
+//      --debug = optional, if --debug is specified, then debug logging will be enabled.
 
-const testfile = process.argv[2];
+const targetFilename = process.argv[2];
 const config = require(process.argv[3] || './sampleConfig.js');
 const debug = process.argv[4] || '';
 
 const flags = {
-    testfile,
+    targetFilename,
     config,
     debug
 };
