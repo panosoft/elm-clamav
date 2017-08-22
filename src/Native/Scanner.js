cfbc1d1a5548900c365047e26679880daea459cf
Don Gaudino <don@panosoft.com>
2017-08-22 11:18:51 -0700

    Changed to return success when scanning a zero length buffer.

--- src/Native/Scanner.js
+++ src/Native/Scanner.js
@@ -8,18 +8,23 @@ var _panosoft$elm_clamav$Native_Scanner = function() {
             try {
                 config.debug ? console.log('NATIVE --', {config: config, name: name, buffer_length: buffer.length,
                     bufferAsString: buffer.toString('hex', 0, (buffer.length > 80 ? 80 : buffer.length)), partialBufferDisplay: (buffer.length > 80)}) : null;
-                const bufferStream = new stream.PassThrough();
-                bufferStream.end(buffer);
-                clamav.createScanner(config.clamavPort, config.clamavHost).scan(bufferStream, (err, object, malicious) => {
-                    config.debug ? console.log('NATIVE --', {name: name, err: err, malicious: malicious}) : null;
-                    callback(err
-                        ? fail(_elm_lang$core$Native_Utils.Tuple2(name, {message: 'Error scanning \'' + name + '\': ' + err.message, virusName: _elm_lang$core$Maybe$Nothing}))
-                        : (malicious
-                            ? fail(_elm_lang$core$Native_Utils.Tuple2(name, {message: 'Virus found scanning \'' + name + '\': ' + malicious, virusName: _elm_lang$core$Maybe$Just(malicious)}))
-                            : succeed(name)
-                          )
-                        )
-                    });
+                if (buffer.length > 0) {
+                    const bufferStream = new stream.PassThrough();
+                    bufferStream.end(buffer);
+                    clamav.createScanner(config.clamavPort, config.clamavHost).scan(bufferStream, (err, object, malicious) => {
+                        config.debug ? console.log('NATIVE --', {name: name, err: err, malicious: malicious}) : null;
+                        callback(err
+                            ? fail(_elm_lang$core$Native_Utils.Tuple2(name, {message: 'Error scanning \'' + name + '\': ' + err.message, virusName: _elm_lang$core$Maybe$Nothing}))
+                            : (malicious
+                                ? fail(_elm_lang$core$Native_Utils.Tuple2(name, {message: 'Virus found scanning \'' + name + '\': ' + malicious, virusName: _elm_lang$core$Maybe$Just(malicious)}))
+                                : succeed(name)
+                              )
+                            )
+                        });
+                }
+                else {
+                    callback(succeed(name));
+                }
             }
             catch (error) {
                 callback(fail(_elm_lang$core$Native_Utils.Tuple2(name, {message: 'Error scanning \'' + name + '\': ' + err.message, virusName: _elm_lang$core$Maybe$Nothing})));
